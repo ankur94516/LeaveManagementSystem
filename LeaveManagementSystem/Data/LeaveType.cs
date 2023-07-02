693651ea8d9d4610c20fa1233090c06f9cd6285b
ankur94516 <9410829640>
2023-07-02 15:49:46 +0530

    Add project files.

---
+++ LeaveManagementSystem/Data/LeaveType.cs
@@ -0,0 +1,10 @@
+﻿namespace LeaveManagementSystem.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        public int NumberOfDays { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
